dc94cafd404babfe8f3f8653bd534a71d335711b
Rishi Kothari <rishi2019@wgdavis.ca>
2019-06-15 14:44:19 -0400

    Updates

---
+++ precache-manifest.096ada3dca625992e9c2533ff20e5870.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c4168da1c20f6160af54d0227b2e4db",
+    "url": "/reactodo/index.html"
+  },
+  {
+    "revision": "c1d2c343dff355faf970",
+    "url": "/reactodo/static/css/main.a23ca4f4.chunk.css"
+  },
+  {
+    "revision": "856b1bc451544e96eaac",
+    "url": "/reactodo/static/js/2.2a430b7d.chunk.js"
+  },
+  {
+    "revision": "c1d2c343dff355faf970",
+    "url": "/reactodo/static/js/main.d1f02362.chunk.js"
+  },
+  {
+    "revision": "05b3bcf9592b26d303bb",
+    "url": "/reactodo/static/js/runtime~main.fdba5a20.js"
+  }
+]);
